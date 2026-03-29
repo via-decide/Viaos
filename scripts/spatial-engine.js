f0a651b941e21031502c4ce1c311eeeafc24dd9b
via-decide <via.decide@gmail.com>
2026-03-29 17:46:29 +0530

    Update spatial-engine.js

--- scripts/spatial-engine.js
+++ scripts/spatial-engine.js
@@ -1,55 +1,41 @@
-// spatial-engine.js
-// Via-OS Spatial Engine
+class SpatialEngine{
 
-class SpatialEngine {
-  constructor() {
-    this.nodes = {};
-    this.macros = this.createMacroRegistry();
-  }
+constructor(){
 
-  createMacroRegistry() {
-    const macros = {
-      "0,0|0,1|1,0": "decision-core",
-      "1,0|1,1|2,0": "creator-studio",
-      "-1,-1|-1,0|-1,1": "utility-subsystem",
-      "2,2|2,3|3,2": "analytics-bay",
-    };
-
-    if (window.masterKeySigil) {
-      macros[window.masterKeySigil] = "root";
-    }
-
-    return macros;
-  }
-
-  registerNode(id, data) {
-    this.nodes[id] = data;
-  }
-
-  getNode(id) {
-    return this.nodes[id];
-  }
-
-  detectMacro(pattern) {
-    return this.macros[pattern] || null;
-  }
-
-  connectNodes(source, target) {
-    if (!this.nodes[source] || !this.nodes[target]) {
-      console.warn("Invalid node connection:", source, target);
-      return;
-    }
-
-    if (!this.nodes[source].outputs) {
-      this.nodes[source].outputs = [];
-    }
-
-    this.nodes[source].outputs.push(target);
-  }
-
-  exportGraph() {
-    return JSON.stringify(this.nodes, null, 2);
-  }
+this.nodes={}
+this.macros={
+"0,0|0,1|1,0":"decision-core",
+"1,0|1,1|2,0":"creator-studio",
+"-1,-1|-1,0|-1,1":"utility-subsystem",
+"2,2|2,3|3,2":"analytics-bay"
 }
 
-window.SpatialEngine = new SpatialEngine();
+}
+
+registerNode(id,data){
+
+this.nodes[id]=data
+
+}
+
+connect(a,b){
+
+if(!this.nodes[a]||!this.nodes[b]) return
+
+if(!this.nodes[a].out){
+this.nodes[a].out=[]
+}
+
+this.nodes[a].out.push(b)
+
+}
+
+export(){
+
+return JSON.stringify(this.nodes,null,2)
+
+}
+
+}
+
+window.SpatialEngine=new SpatialEngine()
